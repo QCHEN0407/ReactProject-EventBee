3888cb6f76aa231dafec9ce09c2178efa142f0dc
Qingwei <qchen6060@gmail.com>
2021-04-23 02:30:08 -0400

    phase3 finished

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -4,4 +4,20 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+
+
+
 module.exports = router;
