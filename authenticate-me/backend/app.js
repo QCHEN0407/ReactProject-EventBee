e4b2beac5ffaa71812998fd9e8e02afb68019146
Qingwei <qchen6060@gmail.com>
2021-04-23 01:07:11 -0400

    phase2 done

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -8,6 +8,7 @@ const routes = require('./routes');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+const { ValidationError } = require('sequelize');
 
 const app = express();
 app.use(morgan('dev'));
@@ -36,5 +37,35 @@ if (!isProduction) {
   );
    //use routes after all the middleware
    app.use(routes);
+
+   app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+// Process sequelize errors
+    app.use((err, _req, _res, next) => {
+        // check if error is a Sequelize error:
+        if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+        }
+        next(err);
+    });
+
+    app.use((err, _req, res, _next) => {
+        res.status(err.status || 500);
+        console.error(err);
+        res.json({
+          title: err.title || 'Server Error',
+          message: err.message,
+          errors: err.errors,
+          stack: isProduction ? null : err.stack,
+        });
+      });
+
 
   module.exports = app;
