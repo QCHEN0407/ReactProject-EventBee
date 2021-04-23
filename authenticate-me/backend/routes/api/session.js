e8c6f95af98c630a9ec504a1d52c6fecb67b55cd
Qingwei <qchen6060@gmail.com>
2021-04-23 12:58:24 -0400

    phase4 done

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -38,6 +38,20 @@ router.post(
     }
   );
 
+  // Restore session user
+  router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({});
+    }
+  );
+
 // ...
 
 module.exports = router;
