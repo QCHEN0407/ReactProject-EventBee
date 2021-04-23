fdeb5e2d4aa2311589152d1d8d0f0fd2f286aba5
Qingwei <qchen6060@gmail.com>
2021-04-23 02:54:12 -0400

    on User signup API Route

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
