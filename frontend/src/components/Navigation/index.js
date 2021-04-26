289ee5f65ecabf0bb8fde6e5f9e1a0c84fbdbb11
Qingwei <qchen6060@gmail.com>
2021-04-26 15:13:25 -0400

    almost finished nav-bar styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -22,12 +22,22 @@ function Navigation({ isLoaded }){
   }
 
   return (
-    <ul>
-      <li>
-        <NavLink exact to="/">Home</NavLink>
-        {isLoaded && sessionLinks}
-      </li>
-    </ul>
+    <div className='navbar'>
+      <div className='nav_content'>
+        <div className='navbar_left'>
+          <h1>eventbee</h1>
+          <img className='navbar_logo' src='/imgs/300.jpeg' alt="" onClick="location.href='/'"/>
+          <input className='navbar_search' type='search' laceholder="Search your Event" />
+        </div>
+        <div className='navbar_right'>
+          <div className='navbar_btn'>
+            <button className='navbar_btn_login'> Host an event </button>
+            <button className='navbar_btn_login'> Help </button>
+            <button className='navbar_btn_login'> Log In </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
