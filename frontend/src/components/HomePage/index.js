264968d5320bf8f27cf8c6f6306f430f71510e47
Qingwei <qchen6060@gmail.com>
2021-04-26 19:14:23 -0400

    getTicketBTN

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -11,6 +11,13 @@ function HomePage({isLoaded}) {
             <h1>The event for event creators is here</h1>
             <p>Introducing RECONVENE, a free, two-day virtual networking and skillsharing summit about the future of events. Register now to explore where the industry is heading — and where you’d like to take it.</p>
         </div>
+        <div classname="bestOnline">
+            <div className="greySquare"></div>
+            <h1 className="orangeText">Discover the best</h1>
+            <h1 className="blackText">online events</h1>
+            <button className="getTicketsBtn" >Get tickets</button>
+
+        </div>
     </div>
 
  )
