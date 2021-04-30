2498b68dd93bdebb90f5b77f505551c442b924b4
Qingwei <qchen6060@gmail.com>
2021-04-30 15:13:17 -0400

    making ticket modal select option

--- frontend/src/components/TicketPageModal/index.js
+++ frontend/src/components/TicketPageModal/index.js
@@ -20,25 +20,29 @@ function TicketPageModal() {
           <div className='eventTitle'>
             {event.title}
           </div>
-          <div className="ticketList">
-            <form className='ticketpagemodal_form' onSubmit={handleSubmit}>
-              {tickets?.map(ticket => {return (
-                  <div className="ticketItem">
-                    <div>
-                      <div>{ticket.title}</div>
-                      <div>${ticket.price}</div>
-                      <div>{ticket.description}</div>
-                    </div>
-                    <button id={`button_ticket_${ticket.id}`} className="selectQuantityButton">
-                        select quantity
-                    </button>
+
+          <form className='ticketpagemodal_form' onSubmit={handleSubmit}>
+            {tickets?.map(ticket => {return (
+                <div className="ticketItem">
+                  <div>
+                    <div>{ticket.title}</div>
+                    <div>${ticket.price}</div>
+                    <div>{ticket.description}</div>
                   </div>
-              )})}
+                  <select id="" className="selectQuantityButton">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                  </select>
+                </div>
+            )})}
 
-              <button className="checkoutBtn" type="submit">Checkout</button>
-            </form>
+            <button className="checkoutBtn" type="submit">Checkout</button>
+          </form>
 
-          </div>
+
         </div>
         <div className="rightTicketContainer">
           <div className="ticket-img">
